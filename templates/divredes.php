7564d4040136c6c47198f9b9e5d8d89e38a19e52
Alfredo Rafael Fernandez Zamora <alfredorafael@gmail.com>
2016-11-14 08:41:17 -0600

    Se sustituyeron los iconos a lado del logo for fuentes de font awesome

--- templates/divredes.php
+++ templates/divredes.php
@@ -1,18 +1,42 @@
-<aside id="header-content">
-	<ul id="list-social" class="clearfix" >
-   		<li id="icon-iso9001"><a href="//sgc.unicach.mx/iso/upload/Certificado%20vigente%20Marzo%202014-%20Marzo%202017%20.pdf" target="_blank" title="Certificado ISO 9001:2008">ISO 9001:2008</a></li>
-        <li id="icon-iso14001"><a href="//sgc.unicach.mx/iso/upload/Certificado%20ambiental%20ISO14001.pdf" title="Certificado ISO 14001:2004" target="_blank">ISO 14001:2004</a></li>
-    	<li id="icon-facebook"><a href="//es-es.facebook.com/UNICACH" target="_blank" title="Facebook Oficial">Facebook</a></li>
-        <li id="icon-twitter"><a href="//twitter.com/unicach_press" target="_blank" title="Twitter Official">Twitter</a></li>
-        <li id="icon-email"><a href="//correo.unicach.mx/" target="_blank" title="Acceso al Correo Institucional">Correo UNICACH</a></li>
-        <li id="icon-radio"><a href="//radio.unicach.mx/" target="_blank" title="Radio UNICACH 102.5fm - Sinton&iacute;zanos">Radio UNICACH 102.5 FM</a></li>
-         
-        
-       <!-- <script type="text/javascript">if(typeof wabtn4fg==="undefined"){wabtn4fg=1;h=document.head||document.getElementsByTagName("head")[0],s=document.createElement("script");s.type="text/javascript";s.src="./js/whatsapp-button.js";h.appendChild(s);}</script>
-        
-       <li id="icon-whatsapp" > <a href="whatsapp://send" data-text="Te recomiendo la Universidad de Ciencias y Artes de Chiapas, como tu mejor opción en Chiapas" data-href="https://www.unicach.mx" class="wa_btn wa_btn_s" style="display:none">Compartir</a></li>
-       -->
-    </ul>
+<div class="col-sm-8 heder-redes hidden-xs">
+	<div class="col-sm-1 col-sm-offset-5">
+		<a href="https://www.unicach.mx/_/inter/?lang=en" class="heder-redes-link" title="UNICACH Internacional">
+			<i class="fa fa-globe fa-lg" aria-hidden="true"></i>
+		</a>
+	</div>
+	<div class="col-sm-1">
+		<a href="http://sgc.unicach.mx/iso/upload/Certificado%20vigente%20Marzo%202014-%20Marzo%202017%20.pdf" class="heder-redes-link iso9001Icon" title="Certificado ISO 9001:2008">
+			<i class="fa fa-certificate fa-lg" aria-hidden="true"></i>
+		</a>
+	</div>
+	<div class="col-sm-1">
+		<a href="http://sgc.unicach.mx/iso/upload/Certificado%20ambiental%20ISO14001.pdf" class="heder-redes-link iso14001Icon" title="Certificado ISO 14001:2004">
+			<i class="fa fa-certificate fa-lg" aria-hidden="true"></i>
+		</a>
+	</div>
+	<div class="col-sm-1">
+		<a href="http://es-es.facebook.com/UNICACH" class="heder-redes-link facebookIcon" title="Facebook Oficial" target="_blank">
+			<i class="fa fa-facebook fa-lg" aria-hidden="true"></i>
+		</a>
+	</div>
+	<div class="col-sm-1">
+		<a href="http://twitter.com/unicach_press" class="heder-redes-link twitterIcon" title="Twitter Official" target="_blank">
+			<i class="fa fa-twitter fa-lg" aria-hidden="true"></i>
+		</a>
+	</div>
+	<div class="col-sm-1">
+		<a href="http://correo.unicach.mx" class="heder-redes-link mailIcon" title="Correo Institucional" target="_blank">
+			<i class="fa fa-envelope fa-lg" aria-hidden="true"></i>
+		</a>
+	</div>
+	<div class="col-sm-1">
+		<a href="http://radio.unicach.mx" class="heder-redes-link radioIcon" title="Radio UNICACH 102.5fm" target="_blank">
+			<i class="fa fa-microphone fa-lg" aria-hidden="true"></i>
+		</a>
+	</div>
+	
+</div>
+<aside id="header-content visible-xs-*">
  <script type="text/javascript">if(typeof wabtn4fg==="undefined"){wabtn4fg=1;h=document.head||document.getElementsByTagName("head")[0],s=document.createElement("script");s.type="text/javascript";s.src="./js/whatsapp-button.js";h.appendChild(s);}</script>
         
       <div align="right"><a href="whatsapp://send" data-text="Te recomiendo la Universidad de Ciencias y Artes de Chiapas, como tu mejor opción en Chiapas" data-href="https://www.unicach.mx" class="wa_btn wa_btn_s" style="display:none">Compartir</a></div>
